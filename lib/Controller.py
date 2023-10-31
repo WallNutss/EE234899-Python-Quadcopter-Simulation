fixing the SMC control, the formula should be correct

lib/Controller.py
@@ -21,26 +21,27 @@ def PID(Ts, err, err_prev, err_sum, gains):
     """
     return (gains[0] * err) + (err_sum * gains[1]) + (gains[2] * (err - err_prev)/Ts)
 
-# def SMC(sliding, inert, anglevelocity, lamda, prevAngle):
+# def SMC(sliding, inertia, angles_dot, lamda, angle_error_dot):
 #     K = np.array([0.19,      0,    0, \
 #                     0,    0.19,    0, \
-#                     0,      0,  0.01]).reshape(3,3)
+#                     0,      0,  0.19]).reshape(3,3)
 #     # Let him cook
-#     inert_inv = np.linalg.inv(inert)
+#     inert_inv = np.linalg.inv(inertia)
 
 #     # Part A, the Control Equation for attitude
-#     Iw   = np.dot(inert, anglevelocity) # For the I.[p,q,r]
-#     Invw = np.dot(inert_inv, anglevelocity) # For the I^-1.[p,q,r]
+#     Iw   = np.dot(inertia, angles_dot) # For the I.[p,q,r]
+#     Invw = np.dot(-inert_inv, angles_dot) # For the I^-1.[p,q,r]
 
 #     Inertia_F = np.cross(Invw[:,0], Iw[:,0]).reshape(3,1)
-#     U_eq = inert.dot(Inertia_F - inert_inv.dot(0.004609 * anglevelocity) - lamda.dot(anglevelocity))
+#     U_eq = Inertia_F  + lamda.dot(angle_error_dot)
 #     #U_eq = inert.dot(Inertia_F - lamda.dot(anglevelocity))
 
 #     sliding[0] = sat(sliding[0])
 #     sliding[1] = sat(sliding[1])
 #     sliding[2] = sat(sliding[2])
 
-#     U_SMC = U_eq - K.dot(sliding)
+#     u_smc = (U_eq - K.dot(sliding))
+#     U_SMC = inertia.dot(u_smc)
 
 #     print(U_SMC)
 #     print("-------------------------")
@@ -51,33 +52,38 @@ def SMC(sliding, inertia, angles_dot, lamda, angle_error_dot):
     K = np.array([0.4,      0,    0, \
                    0,      0.4,    0, \
                    0,      0,    0.4]).reshape(3,3)
-    K_2 = np.array([0.8,      0,    0, \
-                     0,      0.8,    0, \
-                     0,      0,    0.8]).reshape(3,3)   
+    K_2 = np.array([0.6,      0,    0, \
+                     0,      0.6,    0, \
+                     0,      0,    0.6]).reshape(3,3)   
     a1 = (inertia[1][1] - inertia[2][2])/inertia[0][0]
     #b1 = 1/inertia[0][0]
     b1 = 0.06/inertia[0][0]
+    #b1 = inertia[0][0]/0.06
 
     a2 = (inertia[2][2] - inertia[0][0])/inertia[1][1]
     #b2 = 1/inertia[1][1]
     b2 = 0.06/inertia[1][1]
+    #b2 = inertia[1][1]/0.06
 
     a3 = (inertia[0][0] - inertia[1][1])/inertia[2][2]
     #b3 = 1/inertia[2][2]
-    b3 = 0.06/inertia[2][2]
-
+    b3 = 1/inertia[2][2]
+    #b3 = inertia[2][2]/0.06
     # Roll Control
     #U_2 = inertia[0][0]*( K[0][0]*sat(sliding[0]) - a1*angles_dot[1]*angles_dot[2] + lamda[0][0]*angle_error_dot[0] )
     #U_2 = (1/b1)*( K[0][0]*sat(sliding[0]) - a1*angles_dot[1]*angles_dot[2] + lamda[0][0]*angle_error_dot[0] )
-    U_2 = (1/b1)*( K[0][0]*sat(sliding[0]) + K_2[0][0]*sliding[0] - a1*angles_dot[1]*angles_dot[2] - lamda[0][0]*angle_error_dot[0] )
+    #U_2 = (1/b1)*( K[0][0]*sat(sliding[0]) + K_2[0][0]*sliding[0] - a1*angles_dot[1]*angles_dot[2] - lamda[0][0]*angle_error_dot[0] )
+    U_2 = (1/b1)*( K[0][0]*np.sign(sliding[0]) + K_2[0][0]*sliding[0] - a1*angles_dot[1]*angles_dot[2] + lamda[0][0]*angle_error_dot[0] )
     # Pitch Control
     #U_3 = inertia[1][1]*( K[1][1]*sat(sliding[1]) - a2*angles_dot[0]*angles_dot[2] + lamda[1][1]*angle_error_dot[1] )
     #U_3 = (1/b2)*( K[1][1]*sat(sliding[1]) - a2*angles_dot[0]*angles_dot[2] + lamda[1][1]*angle_error_dot[1] )
-    U_3 = (1/b2)*( K[1][1]*sat(sliding[1]) + K_2[1][1]*sliding[1] - a2*angles_dot[0]*angles_dot[2] - lamda[1][1]*angle_error_dot[1] )
+    #U_3 = (1/b2)*( K[1][1]*sat(sliding[1]) + K_2[1][1]*sliding[1] - a2*angles_dot[0]*angles_dot[2] - lamda[1][1]*angle_error_dot[1] )
+    U_3 = (1/b2)*( K[1][1]*np.sign(sliding[1]) + K_2[1][1]*sliding[1] - a2*angles_dot[0]*angles_dot[2] + lamda[1][1]*angle_error_dot[1] )
     # Yaw Control
     #U_4 = inertia[2][2]*( K[2][2]*sat(sliding[2]) - a3*angles_dot[0]*angles_dot[1] + lamda[2][2]*angle_error_dot[2] )
     #U_4 = (1/b3)*( K[2][2]*sat(sliding[2]) - a3*angles_dot[0]*angles_dot[1] + lamda[2][2]*angle_error_dot[2] )
-    U_4 = (1/b3)*( K[2][2]*sat(sliding[2]) + K_2[2][2]*sliding[2] - a3*angles_dot[0]*angles_dot[1] - lamda[2][2]*angle_error_dot[2] )
+    #U_4 = (1/b3)*( K[2][2]*sat(sliding[2]) + K_2[2][2]*sliding[2] - a3*angles_dot[0]*angles_dot[1] - lamda[2][2]*angle_error_dot[2] )
+    U_4 = (1/b3)*( K[2][2]*np.sign(sliding[2]) + K_2[2][2]*sliding[2] - a3*angles_dot[0]*angles_dot[1] + lamda[2][2]*angle_error_dot[2] )
 
 
     '''

lib/quadcopter.py
@@ -96,6 +96,15 @@ class quadcopter:
         self.y_err_sum = 0.0
         self.z_err_sum = 0.0
 
+        # For SMC
+        self.phi_des_prev = 0.0
+        self.theta_des_prev = 0.0
+        self.psi_des_prev = 0.0
+
+        self.phi_des_prev_dot = 0.0
+        self.theta_des_prev_dot = 0.0
+        self.psi_des_prev_dot = 0.0
+
         # For Logs Purpose
         self.timeLogs  = np.array([0.0])
         self.angleLogs = np.array([0.0, 0.0, 0.0])
@@ -126,15 +135,15 @@ class quadcopter:
         R   = bRe.transpose()
         
         #Derivative State 1 to 3, the velocity, because past state define the future stat,in this function all the disturbance wether its internal of external is put down here
-        # if self.Time > 10:
-        #     try:
-        #         self.dstate[0:3] = self.state[3:6] + self.wind[self.iter,:].reshape(3,1)
-        #     except:
-        #         self.dstate[0:3] = self.state[3:6]
-        #     #self.dstate[0:3] = self.state[3:6] + np.array([2.0, 2.0,2.0]).reshape(3,1)
-        # else:
-        #     self.dstate[0:3] = self.state[3:6]
-        self.dstate[0:3] = self.state[3:6]
+        if self.Time > 10:
+            try:
+                self.dstate[0:3] = self.state[3:6] + self.wind[self.iter,:].reshape(3,1)
+            except:
+                self.dstate[0:3] = self.state[3:6]
+            #self.dstate[0:3] = self.state[3:6] + np.array([2.0, 2.0,2.0]).reshape(3,1)
+        else:
+            self.dstate[0:3] = self.state[3:6]
+        #self.dstate[0:3] = self.state[3:6]
 
         # Calculation of Dynamics Translation Motion of the Drone which is the Acceleration of the Model
         self.dstate[3:6] = (- self.g * np.array([0.0, 0.0, 1.0]).reshape(3,1)) + \
@@ -308,12 +317,19 @@ class quadcopter:
         self.theta_err = self.theta_des - theta
         self.psi_err = self.psi_des - psi
 
+        # Getting the derivative of desired (Will come back later for fixing)
+        angles_desired_dot = np.array([(self.phi_des - self.phi_des_prev)/self.Ts, (self.theta_des - self.theta_des_prev)/self.Ts, (self.psi_des - self.psi_des_prev)/self.Ts]).reshape(3,1)
+        angles_desired_doubledot = np.array([(angles_desired_dot[0][0] - self.phi_des_prev_dot)/self.Ts, 
+                                            (angles_desired_dot[1][0] - self.theta_des_prev_dot)/self.Ts, 
+                                            (angles_desired_dot[2][0] - self.psi_des_prev_dot)/self.Ts ]).reshape(3,1)
+        print(angles_desired_doubledot)
+
         # Combining them in one array
         angle_error = np.array([self.phi_err, self.theta_err, self.psi_err]).reshape(3,1)
         angle_error_dot = np.array([(self.phi_err - self.phi_err_prev)/self.Ts, (self.theta_err - self.theta_err_prev)/self.Ts, (self.psi_err - self.psi_err_prev)/self.Ts]).reshape(3,1)
         
-        # Calculation from the Special Transfer Matrix for Angular Rates (This make conversion from Body Frame to Inertial Frame)
-        angles_dot = np.dot(SpecialR(self.state[6:9]), self.state[9:12])
+        # Calculation from the Special Transfer Matrix for Angular Rates (This make conversion from Body Frame to Inertial Frame), this is angulare rates on Inertial Frame Perspective
+        angles_dot = np.dot(SpecialR(self.state[6:9]), self.state[9:12]) # Kecepatan perubahan sudut di euler angles ({EF})
 
         # Calculation for Sliding Surface
         slidingsurface = angle_error_dot + self.lamda.dot(angle_error)
@@ -338,6 +354,15 @@ class quadcopter:
         self.phi_err_prev = self.phi_err
         self.theta_err_prev = self.theta_err
         self.psi_err_prev = self.psi_err
+
+        # Updating Error Desired Value
+        self.phi_des_prev = self.phi_des
+        self.theta_des_prev = self.theta_des
+        self.psi_des_prev = self.psi_des
+
+        self.phi_des_prev_dot = angles_desired_dot[0][0]
+        self.theta_des_prev_dot = angles_desired_dot[1][0]
+        self.psi_des_prev_dot = angles_desired_dot[2][0]
 
         # Updating Control Blocks
         self.Thrust = self.U[0]